30e30b5be5c614bd4225fb40f6b03730c8d5b24c
lambrosapoel <mikasack3rman@gmail.com>
2020-12-11 06:37:43 +0000

    did this

--- project_analysis/munge/01-A.R
+++ project_analysis/munge/01-A.R
@@ -1,7 +1,21 @@
 # Example preprocessing script.
 
 
+
+
 video1=cyber.security.3_video.stats
 dataset1 = data.frame(video1[,c("desktop_device_percentage")])
+video2=cyber.security.4_video.stats
+dataset2 = data.frame(video2[,c("desktop_device_percentage")])
+video3=cyber.security.5_video.stats
+dataset3 = data.frame(video3[,c("desktop_device_percentage")])
+video4=cyber.security.6_video.stats
+dataset4 = data.frame(video4[,c("desktop_device_percentage")])
+
+
+
+
+
 dataset1
+
 
